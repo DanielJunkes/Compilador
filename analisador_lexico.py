b383507f71874cfb618b83f6728b6bd8be16379a
DanielJunkes <danielmafioleti2@gmail.com>
2024-04-02 16:40:01 -0300

    algumas mudanças

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -15,7 +15,6 @@ palavras_reservadas = {'while': 1, 'void': 2, 'string': 3, 'return': 4, 'main': 
 atribuidores_parentizacao = {'>>': 26, '>=': 27, '>': 28, '==': 29, '=': 30, '<=': 31, '<<': 32, '<': 33, '++': 34, '+': 35, '{': 36,
                              '}': 37, ';': 38, ':': 39, '/': 40, ',': 41, '*': 42, '(': 43, ')': 44, '$': 45, '!=': 46, '--': 47, '-': 48}
 
-
 def analisar():
     #pega o numero de linhas
     linhas = textBox.index('end-1c').split('.')[0]   
@@ -23,12 +22,15 @@ def analisar():
         lexema = ''
         #pega o texto que esta na linha
         codigo = textBox.get(f'{i}.0', f'{i}.end')
+        print(codigo)
         #percorre o texto da linha
         for j in range(len(codigo)):
+            print(j)
             if codigo[j] != ' ':
                     lexema = lexema + codigo[j]
             else:
                 lexema = ''
+            print(lexema)
             #verifica se o lexema esta dentro das palavras reservadas
             if lexema in palavras_reservadas:
                 for key, value in palavras_reservadas.items():
@@ -36,9 +38,12 @@ def analisar():
                         tokens.append(value)
                         codigos.append(key)
                         linha.append(i)
+                        lexema = ''
+                        break
             #verifica se o lexama esta dentro dos atribuidores e parentizacao
             elif lexema in atribuidores_parentizacao:
                 if j+1 < len(codigo):
+                    #verificacao para atribuidores com mais de um caracter
                     if lexema == '>':
                         if codigo[j+1] == '>' or codigo[j+1] == '=':
                             lexema += codigo[j+1]
@@ -63,12 +68,17 @@ def analisar():
                         if codigo[j+1] == '=':
                             lexema += codigo[j+1]
                             j += 1
+                    if lexema == '/':
+                        if codigo[j+1] == '/':
+                            break
                 for key, value in atribuidores_parentizacao.items():
                     if key == lexema:
                         tokens.append(value)
                         codigos.append(key)
                         linha.append(i)
-                        
+                        lexema = ''
+                        break
+                    
     for i in range(len(tokens)):
         print(f'Token: {tokens[i]} - Lexema: {codigos[i]} - Linha: {linha[i]}')
 
