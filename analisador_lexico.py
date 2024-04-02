5abee0980b3c4d53310210636af89fd812514531
DanielJunkes <danielmafioleti2@gmail.com>
2024-04-02 12:43:43 -0300

    prototipo funcionando

--- analisador_lexico.py
+++ analisador_lexico.py
@@ -1,19 +1,25 @@
 import customtkinter as ctk
 
-palavras_reservadas = [('while', 1), ('void', 2), ('string', 3), ('return', 4), ('main', 11), ('integer', 13), ('inicio', 14), ('if', 15), 
-                       ('for', 17), ('float', 18), ('fim', 19), ('else', 20), ('do', 21), ('count', 22), ('cin', 23), ('char', 24), 
-                       ('callfuncao', 25), ('>>', 26), ('>=', 27), ('>', 28), ('==', 29), ('=', 30), ('<=', 31), ('<<', 32), ('<', 33), 
-                       ('++', 34), ('+', 35), ('}', 36), ('{', 37), (';', 38), (':', 39), ('/', 40), (',', 41), ('*', 42), (')', 43), 
-                       ('(', 44), ('$', 45), ('!=', 46), ('--', 47), ('-', 38)]
+palavras_reservadas = [{'while': 1}, {'void': 2}, {'string': 3}, {'return': 4}, {'main': 11}, {'integer': 13}, {'inicio': 14}, {'if': 15}, 
+                       {'for': 17}, {'float': 18}, {'fim': 19}, {'else': 20}, {'do': 21}, {'count': 22}, {'cin': 23}, {'char': 24}, 
+                       {'callfuncao': 25}, {'>>': 26}, {'>=': 27}, {'>': 28}, {'==': 29}, {'=': 30}, {'<=': 31}, {'<<': 32}, {'<': 33}, 
+                       {'++': 34}, {'+': 35}, {'}': 36}, {'{': 37}, {';': 38}, {':': 39}, {'/': 40}, {',': 41}, {'*': 42}, {'}': 43}, 
+                       {'{': 44}, {'$': 45}, {'!=': 46}, {'--': 47}, {'-': 48}]
 
-codigo = ''
-
-def pegar_text():
-    global codigo
+def analisar():
     codigo = textBox.get("1.0", "end-1c")
-    
-def printar_text():
-    print(codigo)
+    lexema = ''
+    for i in range(len(codigo)):
+        if codigo[i] != ' ':
+            lexema = lexema + codigo[i]
+        else:
+            lexema = ''
+        print(lexema)
+        for i in range(len(palavras_reservadas)):
+            for key, value in palavras_reservadas[i].items():
+                if key == lexema:
+                    print(value)
+        
 
 app = ctk.CTk()
 
@@ -25,7 +31,7 @@ app.grid_rowconfigure((0,1,2,3), weight=1)
 textBox = ctk.CTkTextbox(app)
 textBox.grid(row=0, column=0, rowspan=3, columnspan=3, sticky="nsew", padx=10, pady=10)
 
-btnAnalisar = ctk.CTkButton(app, text="Analisar", command=pegar_text)
+btnAnalisar = ctk.CTkButton(app, text="Analisar", command=analisar)
 btnAnalisar.grid(row=3, column=1, sticky="nsew", padx=10, pady=10)
 
 app.mainloop()
